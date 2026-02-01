6e8dcb51b49174d40c923af65194c58d1914552f
Mohd Sufian <sehzank@gmail.com>
2026-02-01 17:17:03 +0530

    Add variables for AWS region, project name, and VPC CIDR

--- variables.tf
+++ variables.tf
@@ -1,1 +1,18 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "project_name" {
+  description = "Project name"
+  type        = string
+  default     = "paysecure"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR block"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
